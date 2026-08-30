555fa628fecee5db3334eff95014768729090a0b
Warren Wang <warren.yung.wang@gmail.com>
2026-08-30 17:03:46 +0800

    Add bl_info to get rid of warning in console when plugin is loaded at launch

--- mmd_tools/__init__.py
+++ mmd_tools/__init__.py
@@ -14,6 +14,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+bl_info = {
+    "name": "MMD Tools",
+    "author": "MMD",
+    "version": (4, 5, 13),
+    "blender": (5, 2, 0),
+    "description": "Utility tools for MMD model editing",
+}
+
 import os
 
 from . import auto_load
